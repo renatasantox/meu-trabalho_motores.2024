0c387624a2ec4de58b42c2b31dd64e67e2710e52
renatasantox <renatarafaelapereiradejesis@gmail.com>
2024-08-27 11:08:37 -0300

    Nova plataforma

--- Meu Game 3D/Assets/Scripts/Player.cs
+++ Meu Game 3D/Assets/Scripts/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,22 @@ public class Player : MonoBehaviour
 {
     public int velocidade = 10;
     public Rigidbody rb;
+    public int forcaPulo = 10;
+    public bool noChao = true;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("start");
         TryGetComponent(out rb);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Chão")
+        {
+            noChao = true;
+        }    
     }
 
     // Update is called once per frame
@@ -24,6 +35,11 @@ public class Player : MonoBehaviour
 
         UnityEngine.Vector3 direcao = new Vector3(h, 0, v);
         rb.AddForce(direcao * velocidade * Time.deltaTime, ForceMode.Impulse);
+        if (Input.GetKeyDown(KeyCode.Space) && noChao == true)
+        {
+            rb.AddForce(Vector3.up * forcaPulo, ForceMode.Impulse);
+            noChao = false;
+        }
         if (transform.position.y < -5)
         {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
